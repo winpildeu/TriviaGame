6e239f580f5636b7d341c6337babc8e843f3087e
Winfield H <winterfreshness@gmail.com>
2018-11-16 22:22:43 -0500

    styles fixed. start game button added.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,40 +13,52 @@ const questionBank = [{
 
 // counter to move to the next question
 let q = 0;
+let correct = 0;
 
 let timeLeft;
-let timerDisplay = $("#timer");
-let questionDisplay = $("#question");
-let answerDisplay = $("#answers");
+let clear;
+const timerDisplay = $("#timer");
+const questionDisplay = $("#question");
+const answerDisplay = $("#answers");
 
 // ========== FUNCTIONS ==========
 
 // Generic function for capturing the choice from the data-attribute
 function displayAnswer() {
     // reset the time
-    clearTimeout(timeLeft);
-
-    // empty out the divs
-    $("#timer").empty();
-    $("#question").empty();
-    $("#answers").empty();
-
+    clearInterval(timeLeft);
+    
     // log the user's answer
     let userChoice = $(this).attr("data-choice");
     console.log(`User choice: ${userChoice}`);
+    $("#myAns").text(`${userChoice}`);
+    $("#correctAns").text(`${questionBank[q].ans}`);
 
-    // move on to the next question
-    q++;
-    if (q === questionBank.length) {
-        // go to the results page
-        alert(`TIME'S UP!`);
-    }
-    displayQuestion();
+    // show the answer
+    $("#answer").css("display", "block");
+
+    checkAnswer(userChoice);
+    clear = setTimeout(function () {
+
+        // empty out the divs
+        $("#timer").empty();
+        $("#question").empty();
+        $("#answers").empty();
+        // move on to the next question
+        q++;
+        if (q === questionBank.length) {
+            // go to the results page
+            alert(`Number correct: ${correct}`);
+        }
+        displayQuestion();
+    }, 5000);
+
 }
 
 function displayQuestion() {
+    $("#answer").css("display", "none");
     // Show the time left
-    let timer = 30;
+    let timer = 10;
 
     timeLeft = setInterval(function () {
         timer--;
@@ -64,8 +76,18 @@ function displayQuestion() {
     }
 }
 
-// Add an onclick to all generated elements with a class of answer
-$(document).on("click", ".answer", displayAnswer);
+function checkAnswer(userAnswer) {
+    if(userAnswer === questionBank[q].ans) {
+        correct++;
+    }
+}
+// =========== MAIN CODE ===========
 
-// onclick start button to do quiz
-displayQuestion();
+// Add an onclick to all generated elements with a class of answer
+$(function() {
+    $(document).on("click", ".answer", displayAnswer);
+    $("#startButton").on("click", function() {
+        $("#startGame").css("display", "none");
+        displayQuestion();
+    });
+})
