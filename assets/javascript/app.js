02de19b3f8aefe13e615525991aa0222604f7361
Winfield H <winterfreshness@gmail.com>
2018-11-19 22:09:27 -0500

    Fixed some minor logic bugs. Added real questions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,44 @@
 // Array of questions
 const questionBank = [{
-        question: "What fruit do I like?",
-        choices: ["Apple", "Banana", "Cantelope", "Don't know"],
+        question: "Iceberg is a type of what vegetable?",
+        choices: ["cabbage", "cauliflower", "pea", "lettuce"],
+        ans: 3
+    },
+    {
+        question: "If you were eating a Muscat or a Hanepoot what would you be eating?",
+        choices: ["Peach", "Plum", "Grape", "Pear"],
         ans: 2
     },
     {
-        question: "What WHAT?",
-        choices: ["E", "F", "G", "H"],
+        question: "Imperial Gage and Red Gage are varieties of which fruit?",
+        choices: ["Lemon", "Lime", "Peach", "Plum"],
+        ans: 3
+    },
+    {
+        question: "In which continent did Cranberries originate?",
+        choices: ["Africa", "Asia", "Europe", "North America"],
+        ans: 3
+    },
+    {
+        question: "In which country did Avocados originate?",
+        choices: ['Mexico', 'Brazil', 'Italy', 'Spain'],
+        ans: 0
+    },
+    {
+        question: "Jerusalem and Globe are types of which vegetable?",
+        choices: ['Artichoke', 'Bean', 'Celery', 'Kale'],
+        ans: 0
+    },
+    {
+        question: "Kale is noted for its high content of what vitamin?",
+        choices: ['D', 'A', 'K', 'E'],
         ans: 2
     }
 ];
 
 // counter to move to the next question
 let q = 0;
+// counts the number of correct answers
 let correct = 0;
 
 let timeLeft;
@@ -31,8 +57,10 @@ function displayAnswer() {
     // log the user's answer
     let userChoice = $(this).attr("data-choice");
     console.log(`User choice: ${userChoice}`);
-    $("#myAns").text(`${userChoice}`);
-    $("#correctAns").text(`${questionBank[q].ans}`);
+    let answer = questionBank[q].ans;
+
+    $("#myAns").text(`${questionBank[q].choices[userChoice]}`);
+    $("#correctAns").text(`${questionBank[q].choices[answer]}`);
 
     // show the answer
     $("#answer").css("display", "block");
@@ -50,6 +78,8 @@ function displayAnswer() {
         if (q === questionBank.length) {
             // go to the results page
             $("#endGame").css("display", "inline");
+            $("#correct").text(`${correct}`);
+            $("#incorrect").text(`${(q-correct)}`);
         } else {
             displayQuestion();
         }
@@ -60,6 +90,10 @@ function displayAnswer() {
 function displayQuestion() {
     $("#answer").css("display", "none");
     $("#endGame").css("display", "none");
+
+    // reset the timeout
+    clearTimeout(clear);
+
     // Show the time left
     let timer = 10;
 
@@ -68,20 +102,22 @@ function displayQuestion() {
         timerDisplay.html(`${timer}`);
         if (timer === 0) {
             // checkAnswer func. and show the content
-            // displayAnswer();
+            displayAnswer();
         }
     }, 1000);
 
     // Show the question and answer choices
-    questionDisplay.html(`<h1>${questionBank[0].question}</h1>`);
+    questionDisplay.html(`<h1>${questionBank[q].question}</h1>`);
     for (let i = 0; i < questionBank[0].choices.length; i++) {
-        answerDisplay.append($("<button>").addClass("btn btn-warning btn-lg btn-block answer").attr("type", "button").attr("data-choice", i).text(`${questionBank[q].choices[i]}`));
+        answerDisplay.append($("<button>").addClass("btn btn-warning btn-lg btn-block shadow-sm answer").attr("type", "button").attr("data-choice", i).text(`${questionBank[q].choices[i]}`));
     }
 }
 
 function checkAnswer(userAnswer) {
     if (userAnswer === questionBank[q].ans) {
         correct++;
+        console.log(`Number correct: ${correct}`);
+        
     }
 }
 // =========== MAIN CODE ===========
@@ -89,7 +125,7 @@ function checkAnswer(userAnswer) {
 $(function () {
     // Add an onclick to all generated elements with a class of answer
     $(document).on("click", ".answer", displayAnswer);
-    
+
     // Starts the game when the button is clicked
     $("#startButton").on("click", function () {
         $("#startGame").css("display", "none");
@@ -97,8 +133,17 @@ $(function () {
     });
 
     // Resets the game when the button is clicked
-    $("#resetButton").on("click", function() {
+    $("#resetButton").on("click", function () {
         q = 0;
+        correct = 0;
+        // empty out the divs
+        $("#timer").empty();
+        $("#question").empty();
+        $("#answers").empty();
+
+        // clear all timeouts/ intervals
+        clearInterval(timeLeft);
+        clearTimeout(clear);
         displayQuestion();
     });
 })
